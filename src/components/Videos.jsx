7207613f7d420fbf6a3fae74237f5fc6f8966102
LiamKT0910 <bigboy09101994@gmail.com>
2023-02-28 00:23:53 -0600

    video commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChanelCard } from "./";
+
+const Videos = ({ videos }) => {
+  
+
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChanelCard chanelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
